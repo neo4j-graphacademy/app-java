73db1fe39b3ad3226eae03d9bd0bda2235a086d0
Adam Cowley <adam@adamcowley.co.uk>
2022-10-31 10:39:52 +0000

    Completed challenge: Person Profile

--- src/main/java/neoflix/services/PeopleService.java
+++ src/main/java/neoflix/services/PeopleService.java
@@ -73,9 +73,24 @@ public class PeopleService {
      */
     // tag::findById[]
     public Map<String, Object> findById(String id) {
-        // TODO: Find a user by their ID
+        // Open a new database session
+        try (var session = driver.session()) {
 
-        return people.stream().filter(p -> id.equals(p.get("tmdbId"))).findAny().get();
+            // Get a person from the database
+            var person = session.executeRead(tx -> {
+                        String query = """
+                                    MATCH (p:Person {tmdbId: $id})
+                                    RETURN p {
+                                        .*,
+                                        actedCount: size((p)-[:ACTED_IN]->()),
+                                        directedCount: size((p)-[:DIRECTED]->())
+                                    } AS person
+                                """;
+                        var res = tx.run(query, Values.parameters("id", id));
+                        return res.single().get("person").asMap();
+                    });
+            return person;
+        }
     }
     // end::findById[]
 
@@ -89,9 +104,26 @@ public class PeopleService {
      */
     // tag::getSimilarPeople[]
     public List<Map<String,Object>> getSimilarPeople(String id, Params params) {
-        // TODO: Get a list of similar people to the person by their id
+        // Open a new database session
+        try (var session = driver.session()) {
 
-        return AppUtils.process(people, params);
+            // Get a list of similar people to the person by their id
+            var res = session.executeRead(tx -> tx.run("""
+                    MATCH (:Person {tmdbId: $id})-[:ACTED_IN|DIRECTED]->(m)<-[r:ACTED_IN|DIRECTED]-(p)
+                    RETURN p {
+                        .*,
+                        actedCount: size((p)-[:ACTED_IN]->()),
+                        directedCount: size((p)-[:DIRECTED]->()),
+                        inCommon: collect(m {.tmdbId, .title, type: type(r)})
+                    } AS person
+                    ORDER BY size(person.inCommon) DESC
+                    SKIP $skip
+                    LIMIT $limit
+                    """,Values.parameters("id",id, "skip", params.skip(), "limit", params.limit()))
+                    .list(row -> row.get("person").asMap()));
+
+            return res;
+        }
     }
     // end::getSimilarPeople[]
 
